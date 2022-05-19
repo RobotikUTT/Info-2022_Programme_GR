462b1e4c145ca3b452cfbfd9b34dd625656b4736
Florimond Lalande <38161801+Flowpy@users.noreply.github.com>
2022-05-19 09:15:34 +0200

    [MEGA] Pins.h file complete + WIP on main loop + Added submodule SimpleTimer

--- Code_Mega/Code_Mega.ino
+++ Code_Mega/Code_Mega.ino
@@ -1,9 +1,77 @@
-//Code_Mega.ino
+/*
+ * \file Code_Mega.ino
+ * \brief Holds the Arduino setup and loop functions, which make the robot run.
+*/
+
+// #include <SimpleTimer.h>
+
+#include "include/pins.h"
+#include "include/parameters.h"
+#include "lib/Arduino-SimpleTimer/SimpleTimer/SimpleTimer.h"
+
+SimpleTimer asserv_timer = SimpleTimer();
+
+void waitForTirette();
+void asservLoop();
 
 void setup() {
-	// put your setup code here, to run once:
+	#ifdef DEBUG
+	Serial.begin(115200);
+	Serial.println(">>> Robot Startup - Robotik 2022 <<");
+	#endif
+	asserv_timer.setInterval(TIMER_MS, asservLoop);
+
+	pinMode(TIRETTE, INPUT_PULLUP);
+	pinMode(LED_RED, OUTPUT);
+	pinMode(LED_GREEN, OUTPUT);
+	pinMode(LED_BLUE, OUTPUT);
+
+	waitForTirette();
 }
 
 void loop() {
-	// put your main code here, to run repeatedly:
+	asserv_timer.run();
+}
+
+void asservLoop() {
+	// robotState.update();
+	// collisions.update();
+	// goalList.processCurrentGoal();  
+}
+
+void waitForTirette() {
+	uint32_t last_millis = 0;
+	bool led_state = false;
+
+	// Wait for Tirette insertion
+	if (!digitalRead(TIRETTE)) {
+		#ifdef DEBUG
+		Serial.println("/!\\ Tirette not in place ! Waiting..");
+		#endif
+		while (!digitalRead(TIRETTE)) {
+			if (millis() - last_millis > 150) {
+				last_millis = millis();
+				digitalWrite(LED_RED, led_state ^= 1);
+			}
+		delay(1);
+		}
+		digitalWrite(LED_RED, led_state = false);
+		delay(300);
+	}
+
+	// Wait for Tirette withdrawal
+	#ifdef DEBUG
+	Serial.println("Waiting Tirette signal to start the match !");
+	#endif
+	while (digitalRead(TIRETTE)) {
+		if (millis() - last_millis > 350) {
+			last_millis = millis();
+			digitalWrite(LED_BLUE, led_state ^= 1);
+		}
+		delay(1);
+	}
+	digitalWrite(LED_BLUE, HIGH);
+	#ifdef DEBUG
+	Serial.println("Gooo !");
+	#endif
 }
---
+++ Code_Mega/include/parameters.h
@@ -0,0 +1,15 @@
+/**
+ * \file parameters.h
+ * \brief All parameters used
+**/
+
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
+
+/* Main loop */
+#define RATE_HZ 200 // Hz
+#define TIMER_MS 1000.0 / (float)RATE_HZ // ms
+#define DEBUG   // Uncomment/Comment to Enable/Disable Debug Prints
+
+
+#endif // PARAMETERS_H
